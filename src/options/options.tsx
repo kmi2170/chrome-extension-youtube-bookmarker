d2c56d9e9df55e86de1116c811586b597387b3c0
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2022-08-21 21:03:58 -0700

    modify option's sentence
    	modified:   src/options/options.tsx

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -12,7 +12,7 @@ const Options = () => {
     setIsAllPages(e.target.checked);
 
     storeOptoins(key, e.target.checked)
-      .then((data) => console.log(data))
+      // .then((data) => console.log(data))
       .catch((error) => console.error(error));
   };
 
@@ -27,7 +27,9 @@ const Options = () => {
       <h1 className="mt-8 ml-8 text-3xl text-black">Options</h1>
       <div className="ml-12 mt-4 text-2xl flex items-center">
         <input type="checkbox" checked={isAllPages} onChange={handleChange} />
-        <label className="ml-3 text-2xl">Always show bookmarks</label>
+        <label className="ml-3 text-2xl">
+          Also show bookmarks on any pages other than YouTube
+        </label>
       </div>
     </div>
   );
